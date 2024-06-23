ffcbda4a3d02c8d58d9c42e22033a0b874930838
djyoon <dbsehwls123@gmail.com>
2024-06-23 19:55:03 +0900

    1. 회원가입 Step 다음 버튼클릭시, 현재 Step 검증안된 항목있는경우 진행안되도록처리

--- src/stores/stepCounter.ts
+++ src/stores/stepCounter.ts
@@ -7,7 +7,11 @@ export const useStepCounterStore = defineStore('stepCounter', () => {
   const count = ref(0);
   const getCount = computed(() => count.value);
   function increment() {
-    count.value = count.value == maxCount.value ? count.value : count.value + 1;
+    const { signUpForm } = useSignUpFormStore();
+    const isValidNextStep = !Object.values(signUpForm).filter(v => v.stepCnt == count.value).find(v => v.isValid === false);
+    if(isValidNextStep){
+      count.value = count.value == maxCount.value ? count.value : count.value + 1;
+    }
   }
   function decrease() {
     count.value = count.value == 0 ? count.value : count.value - 1;
